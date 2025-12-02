4d78d00022141bda87dfe75bb6fd4532b05bdc84
hegik <hegik@me.com>
2025-12-02 12:28:47 +0100

    only supporting countries that are part of Office International du Coin de Terre et des Jardins Familiaux

--- app/src/androidMain/kotlin/de/westnordost/streetcomplete/quests/allotments_plot/AddPlotRef.kt
+++ app/src/androidMain/kotlin/de/westnordost/streetcomplete/quests/allotments_plot/AddPlotRef.kt
@@ -7,6 +7,7 @@ import de.westnordost.streetcomplete.data.osm.mapdata.MapDataWithGeometry
 import de.westnordost.streetcomplete.data.osm.mapdata.filter
 import de.westnordost.streetcomplete.data.osm.osmquests.OsmFilterQuestType
 import de.westnordost.streetcomplete.data.quest.AndroidQuest
+import de.westnordost.streetcomplete.data.quest.NoCountriesExcept
 import de.westnordost.streetcomplete.data.user.achievements.EditTypeAchievement
 import de.westnordost.streetcomplete.osm.Tags
 
@@ -17,10 +18,14 @@ class AddPlotRef : OsmFilterQuestType<String>(), AndroidQuest {
         allotments = plot
         and !ref
     """
+
     override val changesetComment = "Added Ref number to allotments plot"
     override val wikiLink = "Key:ref"
-    override val icon = R.drawable.quest_housenumber // ICON ANFERTIGGEN
+    override val icon = R.drawable.quest_housenumber
     override val achievements = listOf(EditTypeAchievement.CITIZEN)
+    override val enabledInCountries = NoCountriesExcept(
+        "BE", "DK", "DE", "FI", "FR", "GB", "LU", "NL", "NO", "AT", "PL", "SE", "CH", "SK"
+    ) // only countries, that are part of Office International du Coin de Terre et des Jardins Familiaux
 
     override fun getTitle(tags: Map<String, String>) = R.string.quest_allotments_plot_ref
 
